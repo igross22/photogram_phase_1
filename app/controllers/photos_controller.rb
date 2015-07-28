89f68f8b3c323739d98bd43fada2492d8afcd78a
igross22 <ian.j.gross@gmail.com>
2015-07-28 18:49:10 -0500

    Changes as of 7pm, 7/28

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,38 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    photo_index = params['number']
+    @photo_source = Photo.find(photo_index).source
+    @photo_caption = Photo.find(photo_index).caption
+
+    render("photo.html.erb")
+  end
+
+  def edit
+    photo_index = params['number']
+    @photo_source = Photo.find(photo_index).source
+    @photo_caption = Photo.find(photo_index).caption
+
+    render("edit.html.erb")
+  end
+
+  def new_form
+
+    render("new.html.erb")
+  end
+
+  def destroy
+
+    photo_index = params['number']
+    i = Photo.find(photo_index)
+    i.destroy
+    @list_of_photos = Photo.all
+    render("index.html.erb")
+
+  end
 
 end
+
+
+
